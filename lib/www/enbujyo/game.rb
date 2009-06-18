0cba99af0637e054e78f2d2897720679184fba90
holysugar <holysugar@gmail.com>
2009-06-18 09:39:29 +0900

    Bug fix.

--- lib/www/enbujyo/game.rb
+++ lib/www/enbujyo/game.rb
@@ -3,7 +3,8 @@ require 'time'
 module WWW
   class Enbujyo
     class Game
-      attr_reader :player0, :deck0, :location0, :player1, :deck1, :location1
+      attr_reader :player0, :deck0, :location0, :player1, :deck1, :location1,
+        :date, :movie_date
 
       def initialize(p0, p0deck, p0loc, p1, p1deck, p1loc, options = {})
         @player0 = p0
@@ -13,7 +14,8 @@ module WWW
         @deck1 = p1deck
         @location1 = p1loc
 
-        @date = Time.parse(options[:date]) if options[:date]
+        @date = Time.parse(options['date']) if options['date']
+        @movie_date = Date.parse(options['movie_date']) if options['movie_date']
         case options[:p0result]
         when 'lose'
           @winner = 1
